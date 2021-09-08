e3b533f0697964a64a294e5482c37887f5b682d0
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-09-07 19:03:49 -0500

    Add project files.

---
+++ MagicMoose/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MagicMoose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _responses = new ResponseRepository();
+            Console.WriteLine("Ask the Magic Moose a question...");
+            var question = Console.ReadLine();
+            if (question.Length == 0) Environment.Exit(0);
+            if (question.Length > 0) Console.WriteLine(_responses.Reply()); 
+        }
+    }
+}
